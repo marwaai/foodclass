1f47ebed20fd787aaa2badaba79230b7e4b2e19d
marwaai <Marwa.mahmoud.abdallah@gmail.com>
2024-11-25 19:03:47 +0200

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
  echo "BUILD START"
  python3.9  pip install -r requirements.txt
+ python3.9  pip install psycopg2-binary==2.8.4
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
